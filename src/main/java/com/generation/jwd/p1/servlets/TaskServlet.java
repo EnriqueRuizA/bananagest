44189906ed37db439bc67ff20ba965db75f64663
NOxtradamuZ <Arranque 1@a103-pc06>
2017-12-20 17:06:33 +0100

    test2

--- src/main/java/com/generation/jwd/p1/servlets/TaskServlet.java
+++ src/main/java/com/generation/jwd/p1/servlets/TaskServlet.java
@@ -10,39 +10,38 @@ import javax.servlet.http.HttpSession;
 
 import com.generation.jwd.p1.beans.TaskBean;
 
-//@WebServlet("/createtask")
+@WebServlet("/createtask")
 public class TaskServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     public TaskServlet() {
         super();
-        
     }
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		TaskBean task = new TaskBean();
-	    task.setNameTask(request.getParameter("nameTask"));
-	    task.setDescription(request.getParameter("description"));
-	    task.setdateStart(request.getParameter("dateStart"));
-	    task.setdateFinish(request.getParameter("dateFinish"));
-	    task.setNotes(request.getParameter("notes"));
-	    task.setResponsable(request.getParameter("responsable"));
-	    task.setPriority(request.getParameter("priority"));
-	    task.setUrlFile(request.getParameter("urlFile"));
+	
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 TaskBean task = new TaskBean();
+	     task.setNameTask(request.getParameter("nameTask"));
+	     task.setDescription(request.getParameter("description"));
+	     task.setdateStart(request.getParameter("dateStart"));
+	     task.setdateFinish(request.getParameter("dateFinish"));
+	     task.setNotes(request.getParameter("notes"));
+	     task.setResponsable(request.getParameter("responsable"));
+	     task.setPriority(request.getParameter("priority"));
+	     task.setUrlFile(request.getParameter("urlFile"));
 	        
-	    HttpSession session_name= (HttpSession)request.getSession();
+	     HttpSession session_name= (HttpSession)request.getSession();
 	        
-	    if(task.validate() == true) {
-	    	session_name.setAttribute("saveTask", "A new task has been created");
-	    	session_name.setAttribute("task", task.getNameTask());
-	        request.getRequestDispatcher("homeuser.jsp").forward(request, response);
-	    } else {
-	    	session_name.setAttribute("saveTask", "error: a new task has not created");
-	        request.getRequestDispatcher("createtask.jsp").forward(request, response);
-	    }
-	}
+	     if(task.validate() == true) {
+	    	 session_name.setAttribute("saveTask", "A new task has been created");
+	    	 session_name.setAttribute("task", task.getNameTask());
+	         request.getRequestDispatcher("homeuser.jsp").forward(request, response);
+	     } else {
+	    	 session_name.setAttribute("saveTask", "error: a new task has not created");
+	         request.getRequestDispatcher("createtask.jsp").forward(request, response);
+	     }
+	 }
 }
