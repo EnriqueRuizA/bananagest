954c171e7a61a136bae554efd63c78f962c57a49
NOxtradamuZ <Arranque 1@a103-pc06>
2017-12-20 16:37:31 +0100

    Project assembled

--- src/main/java/com/generation/jwd/p1/servlets/LoginServlet.java
+++ src/main/java/com/generation/jwd/p1/servlets/LoginServlet.java
@@ -2,37 +2,39 @@ package com.generation.jwd.p1.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LoginServlet
- */
+import com.generation.jwd.p1.beans.LoginBean;
+
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public LoginServlet() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("login.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		LoginBean loginBean = new LoginBean();
+		loginBean.setUser(request.getParameter("user"));
+		loginBean.setPassword(request.getParameter("password"));
+		
+		HttpSession session = request.getSession();
 
+		if(loginBean.validator() == true) {
+			session.setAttribute("comprobar", "Datos correctos");
+			session.setAttribute("User", loginBean.getUser());
+			request.getRequestDispatcher("homeuser.jsp").forward(request, response);
+		} else {
+			session.setAttribute("comprobar", "Datos incorrectos");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		}	        
+	}
 }
--- src/main/java/com/generation/jwd/p1/servlets/TaskServlet.java
+++ src/main/java/com/generation/jwd/p1/servlets/TaskServlet.java
@@ -2,38 +2,47 @@ package com.generation.jwd.p1.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class TaskServlet
- */
+import com.generation.jwd.p1.beans.TaskBean;
+
+@WebServlet("/createtask")
 public class TaskServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public TaskServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		TaskBean task = new TaskBean();
+	    task.setNameTask(request.getParameter("nameTask"));
+	    task.setDescription(request.getParameter("description"));
+	    task.setdateStart(request.getParameter("dateStart"));
+	    task.setdateFinish(request.getParameter("dateFinish"));
+	    task.setNotes(request.getParameter("notes"));
+	    task.setResponsable(request.getParameter("responsable"));
+	    task.setPriority(request.getParameter("priority"));
+	    task.setUrlFile(request.getParameter("urlFile"));
+	        
+	    HttpSession session_name= (HttpSession)request.getSession();
+	        
+	    if(task.validate() == true) {
+	    	session_name.setAttribute("saveTask", "A new task has been created");
+	    	session_name.setAttribute("task", task.getNameTask());
+	        request.getRequestDispatcher("homeuser.jsp").forward(request, response);
+	    } else {
+	    	session_name.setAttribute("saveTask", "error: a new task has not created");
+	        request.getRequestDispatcher("createtask.jsp").forward(request, response);
+	    }
 	}
-
 }
