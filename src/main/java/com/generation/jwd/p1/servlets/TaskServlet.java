b05b44898abb6f8ad2efdb6a54ee32f8924437d8
NOxtradamuZ <Arranque 1@a103-pc06>
2017-12-20 17:12:54 +0100

    k

--- src/main/java/com/generation/jwd/p1/servlets/TaskServlet.java
+++ src/main/java/com/generation/jwd/p1/servlets/TaskServlet.java
@@ -19,7 +19,6 @@ public class TaskServlet extends HttpServlet {
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 	}
 	
 	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
